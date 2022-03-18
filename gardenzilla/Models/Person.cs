5cdc8c2895b10bfc42aba5ca5e25ee67625f53d2
110678@st-maartenscollege.nl <110678@st-maartenscollege.nl>
2022-03-18 14:07:52 +0100

    verder met de contactpagina

--- gardenzilla/Models/Person.cs
+++ gardenzilla/Models/Person.cs
@@ -3,11 +3,17 @@ namespace gardenzilla.Models
 { 
 public class Person
 {
+    [Required ]
     public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
-    public string Address { get; set; }
-    public string Description { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Description { get; set; }
 }
 }
