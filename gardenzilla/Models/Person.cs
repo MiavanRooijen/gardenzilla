fd2be53fe9410b74b29d389219df5b8a2f4a696c
110678@st-maartenscollege.nl <110678@st-maartenscollege.nl>
2022-05-18 09:26:44 +0200

    contact formulier gefixed

--- gardenzilla/Models/Person.cs
+++ gardenzilla/Models/Person.cs
@@ -5,20 +5,28 @@ namespace gardenzilla.Models
 {
     public class Person
     {
-        [Required(ErrorMessage = "Gelieve uw voornaam in te vullen")]
+        [Required(ErrorMessage = "Voornaam is een verplicht veld")]
         [Display(Name = "Voornaam")]
         public string FirstName { get; set; }
+
         [Required(ErrorMessage = "Achternaam is een verplicht veld")]
-        [Display(Name = "Achternaam")]
+         [Display(Name = "Achternaam")]
         public string LastName { get; set; }
-        [Required(ErrorMessage = "Emailadres is verplicht")]
+       
+        [Required(ErrorMessage = "Emailadres is een verplicht veld")]
         [EmailAddress(ErrorMessage = "Geen geldig email adres")]
+        
         public string Email { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage = "Telefoon is een verplicht veld")]
+        [Display(Name = "Telefoon")]
         public string Phone { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Adres is een verplicht veld")]
+        [Display(Name = "Adres")]
         public string Address { get; set; }
-        [Required(ErrorMessage = "Bericht is verplicht")]
+
+        [Required(ErrorMessage = "Bericht is een verplicht veld")]
         [Display(Name = "Bericht")]
         public string Description { get; set; }
         
