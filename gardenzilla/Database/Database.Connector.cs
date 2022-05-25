d10f2f8fa42642a841338e5cc49092b0a2680ef6
110693@st-maartenscollege.nl <110693@st-maartenscollege.nl>
2022-05-25 10:01:08 +0200

    terrein pagina bewerkt

--- gardenzilla/Database/Database.Connector.cs
+++ gardenzilla/Database/Database.Connector.cs
@@ -48,6 +48,20 @@ namespace gardenzilla.Database
             // return de lijst met namen
             return rows;
         }
+        public static void SavePerson(Person person)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO klant(voornaam, achternaam, email, bericht) VALUES(?voornaam, ?achternaam, ?email, ?bericht)", conn);
 
+                // Elke parameter moet je handmatig toevoegen aan de query
+                cmd.Parameters.Add("?voornaam", MySqlDbType.Text).Value = person.FirstName;
+                cmd.Parameters.Add("?achternaam", MySqlDbType.Text).Value = person.LastName;
+                cmd.Parameters.Add("?email", MySqlDbType.Text).Value = person.Email;
+                cmd.Parameters.Add("?bericht", MySqlDbType.Text).Value = person.Description;
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
